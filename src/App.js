5075345553de5a37a545dbeddf2577131222f037
kenya194 <ernestbv553@gmail.com>
2023-07-05 02:59:30 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+// import './App.css';
+import navbar from "./navbar";
+
+function App() {
+ 
+  return (
+  <div className="APP">
+    <navbar/>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/navbar.js
@@ -0,0 +1,9 @@
+const navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>TODO APP</h1>
+        </nav>
+     );
+}
+
+export default navbar;
