8bef552cb1cf5569a9901464558040d236bb2c16
kenya194 <ernestbv553@gmail.com>
2023-07-13 03:12:21 +0200

    css update

--- src/category.js
+++ src/category.js
@@ -1,21 +1,27 @@
+import Lists from "./lists";
+
 const Category = () => {
     return ( 
         <div>
             <h2>Select a Category</h2>
-            <label>
+
+            <div className="options">
+            <label className="split">
                 <input type="radio" id="category1" value='business'></input>
                 <span className="bubble Business"></span>
                 <div>Business</div>
             </label>
 
 
-            <label>
+            <label className="split">
                 <input type="radio" id="category2" value='personal'></input>
                 <span className="bubble Personal"></span>
                 <div>Personal</div>
             </label>
+            </div>
 
             <input type="submit" value='Add Todo'></input>
+            <Lists />
         </div>
      );
 }
---
+++ src/lists.js
@@ -0,0 +1,8 @@
+const Lists = () => {
+
+    return ( 
+        <h2>Your Added Todos</h2>
+     );
+}
+ 
+export default Lists;
