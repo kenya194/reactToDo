1ad501366c295d0b8ccbaac68d53fb6f18a4bb43
kenya194 <ernestbv553@gmail.com>
2023-07-16 23:57:57 +0200

    commit

--- src/header.js
+++ src/header.js
@@ -2,7 +2,7 @@ function Header() {
     
     return (
         <div className="greetings">
-            <h1 className="title">Hello </h1>
+            <h1 className="title">Hello</h1>
             <input type="text" id="name" placeholder="Name here" ></input>
         </div>
 
