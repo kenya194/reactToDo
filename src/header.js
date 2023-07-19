5f75b74911640fbf9e31a0fe63a84aefcb2e2471
kenya194 <ernestbv553@gmail.com>
2023-07-20 00:42:38 +0200

    saving input

--- src/header.js
+++ src/header.js
@@ -1,4 +1,12 @@
 function Header() {
+
+    let username = localStorage.getItem(" ")
+    let nameInput = document.querySelector("#name")
+    nameInput.value = username;
+
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('', e.target.value)
+    })
     
     return (
         <div className="greetings">
