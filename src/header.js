7bb1bc632804815fbd37895267d358ca2b5197fb
kenya194 <ernestbv553@gmail.com>
2023-07-25 00:25:38 +0000

    .

--- src/header.js
+++ src/header.js
@@ -1,20 +1,17 @@
+import React, { useState } from "react";
+
 function Header() {
 
-    let username = JSON.parse(localStorage.getItem("nameData"))
-    //getting the input value
-    let nameInput = document.querySelector("#name")
-    nameInput.value = username;
-    //letting the username be equal to the input value
+    const [input, setInput] = useState('');
+    const nameChange = e => {
+        setInput(e.target.value);
+    }
 
-    nameInput.addEventListener('change', e => {
-        localStorage.setItem("nameData", e.target.value)
-        // local storage set the name according to the values inputted.
-    })
     
     return (
         <div className="greetings">
             <h1 className="title">Hello</h1>
-            <input type="text" id="name" placeholder="Name here" ></input>
+            <input type="text" id="name" placeholder="Name here" value={input} onChange={nameChange}></input>
         </div>
 
     );
