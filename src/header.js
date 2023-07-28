a20ad6f8404148f1c613be76a1e46a104f2d94a0
kenya194 <ernestbv553@gmail.com>
2023-07-28 01:06:21 +0000

    Name input solved

--- src/header.js
+++ src/header.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Header() {
 
-    const [inputValue, setInput] = useState('');
+    const [inputValue, setInput] = useState( () => {
+    const storedName = localStorage.getItem('Name')
+        return storedName; 
+        });
+        // The above takes the input saves it and returns it. the state actually does this 
     const nameChange = (event) => {
         setInput(event.target.value);
     };
-    localStorage.setItem('Name', inputValue);
-    localStorage.getItem('Name')
+        // this takes the input while being typed and sets it as the new input.
+    useEffect (()=>{
+        localStorage.setItem('Name', inputValue);
+    }, [inputValue]);
+        // the useEffect runs the set item outside the main functions and stores its value 
 
     return (
         <div className="greetings">
