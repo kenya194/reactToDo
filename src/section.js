06bf2ad5231e8b439fbd4abfbece13dfd67d619c
kenya194 <ernestbv553@gmail.com>
2023-07-30 23:54:08 +0000

    update

--- src/section.js
+++ src/section.js
@@ -5,6 +5,10 @@ const Section = () => {
 
 const[yourTodo, setTodo] = useState("");
 
+const inputValue = (e) =>{
+    setTodo(e.target.inputValue)
+}
+
 
 
 
@@ -17,7 +21,7 @@ const[yourTodo, setTodo] = useState("");
             <h2>Add Todo</h2>
             <form className="todoInput">
                 <h4>What is on your mind...</h4>
-                <input type='text' id='content' placeholder="eg. Pack up your bags"></input> 
+                <input type='text' id='content' placeholder="eg. Pack up your bags" onChange={inputValue} value={yourTodo}></input> 
             </form>
             <Category />
         </div>
