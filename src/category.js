0abc8350d08ad46ef8b8ba8496b483b67d724c7b
kenya194 <ernestbv553@gmail.com>
2023-08-02 00:05:55 +0000

    selection of botton

--- src/category.js
+++ src/category.js
@@ -1,20 +1,40 @@
 import Lists from "./lists";
+import React, {useState} from "react";
 
 const Category = () => {
+
+    const[selectedOption, setOption] = useState('business');
+
+    const handleEvent = (e) => {
+            setOption(e.target.value);
+    }
+
+
+
     return ( 
         <div>
             <h2 className="category-head">Select a Category</h2>
 
             <div className="options">
             <label className="split">
-                <input type="radio" id="category1" value='business'></input>
+                <input type="radio"
+                 id="category1" 
+                 value='business' 
+                 onChange={handleEvent}
+                 checked = {selectedOption === 'business'}
+                 ></input>
                 <span className="bubble Business"></span>
                 <div>Business</div>
             </label>
 
 
             <label className="split">
-                <input type="radio" id="category2" value='personal'></input>
+                <input type="radio"
+                 id="category2" 
+                 value='personal'
+                 onChange={handleEvent}
+                 checked = {selectedOption === 'personal'}
+                ></input>
                 <span className="bubble Personal"></span>
                 <div>Personal</div>
             </label>
